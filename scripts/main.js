7c2bf3c788c4484dea50bae4e027d2be1d853ab5
gplanet999 <87784415+gplanet999@users.noreply.github.com>
2021-07-22 17:53:34 -0400

    changed url for getlink

--- scripts/main.js
+++ scripts/main.js
@@ -397,7 +397,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://produce48.github.io/";
+const currentURL = "https://gplanet999.github.io/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
